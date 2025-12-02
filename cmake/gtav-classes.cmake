e694fda3672ff2d6c0a5057f8dd06de8e186bee7
𝐶𝑆𝑌𝑂𝑁 <134310111+CSY0N@users.noreply.github.com>
2025-12-02 01:29:42 +0100

    Add files via upload

---
+++ cmake/gtav-classes.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    gtav_classes
+    GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
+    GIT_TAG        98d4999a7f893d1ebbed7a4852599895afeadef1
+    GIT_PROGRESS TRUE
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+)
+message("GTAV-Classes")
+if(NOT gtav_classes_POPULATED)
+    FetchContent_Populate(gtav_classes)
+endif()
