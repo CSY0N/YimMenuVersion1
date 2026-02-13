2459cc53183c28dc6a10459378d56ffe170d9ce8
Hexlane <deede3771@gmail.com>
2026-02-13 10:11:23 -0500

    Update gtav-classes.cmake

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
-message(STATUS "Fetching GTAV-Classes (sc-news)")
+message(STATUS "Fetching GTAV-Classes (Build_3751.0)")
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Hexlane/GTAV-Classes-1.git
-    GIT_TAG sc-news
+    GIT_TAG Build_3751.0
 )
 FetchContent_MakeAvailable(gtav_classes)
